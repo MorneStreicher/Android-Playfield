ff9c786908ee6528cc5d5ef0af210a0aff097cac
Morne Streicher <MorneStreicher@hotmail.com>
2017-09-18 22:32:40 +0200

    Added a pure Cordova Hello World application.

---
+++ Cordova/HelloWorld1/local_tools/key_store/sign_app.cmd
@@ -0,0 +1,3 @@
+"C:\Program Files\Java\jdk1.8.0_131\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore .\keystore ..\..\platforms\android\build\outputs\apk\android-release-unsigned.apk key0
+
+pause
